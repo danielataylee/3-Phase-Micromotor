6068ad6019d52647490b292409fba17c7a27cb5c
Daniela Tay Lee Sanchez <danielataylee@gmail.com>
2018-05-16 11:25:41 -0700

    Add files via upload

---
+++ ScriptsMotor/I2C_MOTOR_MASTER/I2C_MOTOR_MASTER.ino
@@ -0,0 +1,56 @@
+
+#include <TimeLib.h>
+
+//Master - Comm12 - Channel A (Blue)
+
+/*
+Date: May 2018
+Organization: BCCRC Imaging Unit
+Authors: Varalee Chinbomsom and Daniela Tay Lee Sanchez
+Description: This script is for the master of the I2C connection
+It should read input from the laster trigger output 
+Calculate the number of samples in the sine lookup table from desired frequency
+and pass that to the 3 slaves, so they can each generate a sine wave
+to drive the dual beam micromotor
+*/
+
+#include <Wire.h> // Include the required Wire library for I2C connection
+
+
+//Constants
+#define DAC_RESOLUTION 12
+
+//Variables
+int i = 0; //Iterate through wave values for sine 
+int start = 1; //Like boolean 1, is true to start the slaves
+
+
+//Metohd to configure Arduino
+void setup() 
+{
+  Wire.begin(); //Start the I2C Bus as Master
+  pinMode(DAC0, OUTPUT); //Set DAC0 as output
+  analogWriteResolution(DAC_RESOLUTION); //Set up resolution
+
+  //Send values to slave -> Determine phase offset from this delay and consider 
+  Wire.beginTransmission(0x04); //transmit to device at address 0x04
+  Wire.write(start); //Send signal to start (1)
+  Wire.endTransmission();    //Sop transmitting to slave1
+
+  Wire.beginTransmission(0x06); //transmit to device at address 0x06
+  Wire.write(start); //Send signal to start (1)
+  Wire.endTransmission();    //Sop transmitting to slave2  
+
+  //time.timeset();
+  setSyncInterval(5);
+}
+
+//Method to loop indefinetely
+void loop() 
+{
+
+  
+  //Should read laser trigger output to get frequency and then call method to determine number of samples and pass that to the slaves
+}
+
+//Method to determine number of samples from specified frequency
---
+++ ScriptsMotor/sineLookupTable/sineLookupTable.ino
@@ -0,0 +1,47 @@
+//This script generates a sine lookup table based on number of samples
+
+//Constants
+#define MAX_SAMPLES 4096 //There are 120 elements in array
+#define DAC_RESOLUTION 12
+#define AMPLITUDE 7 //Gets it to 2.5V with 2^8 -> need 3V, but AnalogWrite only 12 bits so can only get to 4096
+#define PI 3.14159265358979323846
+
+//Variables
+int i = 0; //Iterate through wave values for wave 1
+int j=0;
+int offsetdeg = 120; //120 degrees 
+int offset; //Formula: offset degrees * num of samples / 360
+int bitMul = 1<<8; //Is equivalent to 2^8 = 256
+
+//sine wave lookup table
+static int sinewave[MAX_SAMPLES];
+
+void setup() 
+{
+  //Set DACs as outputs
+  pinMode(DAC0, OUTPUT);  
+  analogWriteResolution(DAC_RESOLUTION); //Set up resolution
+  offset = offsetdeg*MAX_SAMPLES/360; //Calculate offset
+
+  //Call method to generate sine lookup table
+  generateSineTable(offsetdeg, sinewave, MAX_SAMPLES, AMPLITUDE);
+}
+
+void loop() 
+{
+    i = (i+1)%MAX_SAMPLES; // Next element, and if reached last, go back to 0
+
+    analogWrite(DAC0, sinewave[i]); //Write every element of sinewave
+}
+
+
+//Method to generate a sine table from the specified offset, and number of samples
+//off is the offset, table is a pointer to the sine lookup table array and samples is the num of elements
+void generateSineTable(int off, int * table, int samples, int a)
+{
+  for (i=off ; i < samples + off ; i++)
+  {
+    table[j++] = a*(round(((sin(2*PI*i/samples))*bitMul))+bitMul);
+  }
+}
+
