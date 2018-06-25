713e9ab1f80f0ee41e3626f08a665c0a931ca0c6
Daniela Tay Lee Sanchez <danielataylee@gmail.com>
2018-06-25 14:50:11 -0700

    Updated reset to act as on/off switch and added prospective change samples code

--- Arduino1Switch/Arduino1Switch.ino
+++ Arduino1Switch/Arduino1Switch.ino
@@ -14,7 +14,7 @@ Description: The script generates a sine lookup table of number of samples SAMPL
 */
 
 //Constants
-#define SAMPLES 1000
+#define MAXSAMPLES 6000
 #define OFFDEG 0
 
 #define DAC_RESOLUTION 12 //12 bits
@@ -22,22 +22,24 @@ Description: The script generates a sine lookup table of number of samples SAMPL
 #define RESET_OUT 31 //This pin outputs high and is used for the switch, only one arduino needs it but the code should be the same so the frequencies are the same among all 3 arduinos 
 #define RESET_IN 23
 #define DAC DAC0
+#define CHG_SAMPLES 35 //Pin high when want to change num of samples
 
 
 //Variables -> Some declared as volatile because used in interrput
+volatile int samples = MAXSAMPLES;
 volatile int i = 0; //Iterate through wave values for sine wave
-volatile int steps = 10000/SAMPLES; //Steps to ramp up
+volatile int steps = 10000/MAXSAMPLES; //Steps to ramp up
 volatile int count = 0; 
 volatile int pulse = 0; 
 volatile int out = 0; //Start skipping this many ticks
 volatile int points = 0; //Track points written to DAC
 volatile int cycles = 100; //Num of cycles to complete on each ramp up
 int j = 0; //Used when generating sine lookup table
-int offset = OFFDEG*SAMPLES/360; //Calculate offset position -> Formula: offset degrees * num of samples / 360
+int offset = OFFDEG*MAXSAMPLES/360; //Calculate offset position -> Formula: offset degrees * num of samples / 360
 int bitMul = (1<<12)/2; //1<<12 Is equivalent to 2^12 = 4096
 
 //Lookup table
-static int wave[SAMPLES]; 
+static int wave[MAXSAMPLES]; 
 
 
 //Method to configure Arduino
@@ -48,15 +50,21 @@ void setup()
    pinMode(RESET_IN, INPUT);
    pinMode(DAC, OUTPUT);   
    pinMode(PIN_SIGNAL, INPUT); 
+   pinMode(CHG_SAMPLES, INPUT);
    //Set up resolution
    analogWriteResolution(DAC_RESOLUTION); 
-   //Call method to generate sine lookup table
-   generateSineTable(offset, wave, SAMPLES); 
+   
+   //Initiliaze USB
+   SerialUSB.begin(2000000); //Native USB port
+   while(!SerialUSB); //While connection is being established
+   //Set num of samples
+   setSamples(SerialUSB.read());
+
    //Set pin as high so used with switch
    digitalWrite(RESET_OUT, HIGH); 
    //Initialize interrupt service routines
    attachInterrupt(digitalPinToInterrupt(PIN_SIGNAL), TRIGGER_ISR, RISING); //Call TRIGGER_ISR when PIN_SIGNAL goes from low to high
-   attachInterrupt(digitalPinToInterrupt(RESET_IN), RESET_ISR, LOW); //Call RESET_ISR when RESET_IN changes
+   attachInterrupt(digitalPinToInterrupt(RESET_IN), RESET_ISR, LOW); //Call RESET_ISR when RESET_IN is low (off)
 }
 
 //Method to generate sine lookup table
@@ -69,6 +77,16 @@ void generateSineTable(int off, int * table, int s)
   j=0;
 }
 
+//Method to change num of samples
+void setSamples(int s)
+{
+  samples = s;//Set new num of samples
+  //calculate new params with new num of samples
+  steps = 10000/samples;
+  offset = OFFDEG*samples/360;
+  //Call method to generate sine lookup table
+  generateSineTable(offset, wave, samples); 
+}
 
 //Method that loops indefinetely
 void loop() 
@@ -85,7 +103,7 @@ void TRIGGER_ISR()
     if ((count%steps == 0) || (pulse <= out)) //Ex: 1000 samples -> steps = 10, Output every multiple of 10 point, or if increasing frequency -> output until pulse = out
     {
       analogWrite(DAC, wave[i]); //Write on DAC
-      i = (i+1)%SAMPLES; //next value in lookup table
+      i = (i+1)%samples; //next value in lookup table
       points++; //Wrote a point
       if (count%steps == 0)
         pulse = 0;
@@ -93,7 +111,7 @@ void TRIGGER_ISR()
     }
 
     //If done number of cycles, restart with next ramp up
-    if (points == (SAMPLES*cycles) && (out < steps))
+    if (points == (samples*cycles) && (out < steps))
     {
       points = 0;
       count = -1; 
@@ -116,5 +134,11 @@ void RESET_ISR()
   pulse=0;
   out=0;
   points=0;
-  
+
+  //If want to change num of sampels
+  if ((digitalRead(CHG_SAMPLES) == HIGH))
+  {
+    //Read the specific num of samples from USB and set num of samples
+    setSamples(SerialUSB.read());
+  }
 }
